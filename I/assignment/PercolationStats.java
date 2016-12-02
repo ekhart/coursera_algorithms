8e639320aa69578758e1a426b6415224ef814ac7
Artur Dębkowski <arturdebkowski@gmail.com>
2016-12-02 15:13:18 +0100

    PercolationStats

---
+++ I/assignment/PercolationStats.java
@@ -0,0 +1,32 @@
+public class PercolationStats {
+
+	// performs trials independent experiments on an n-by-n grid
+	public PercolationStats(int n, int trials) {
+
+	}
+
+	// sample mean of percolation threshold
+	public double mean() {
+
+	}
+
+	// sample standard deviation of percolation threshold
+	public double stddev() {
+
+	}
+
+	// low endpoint of 95% confidence interval
+	public double confidenceLo() {
+
+	}
+
+	// high endpoint of 95% confidence interval
+	public double confidenceHi() {
+
+	}
+
+	// test client (desciribed below)
+	public static void main(String[] args) {
+
+	}
+}
