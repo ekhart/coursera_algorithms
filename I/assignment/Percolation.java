7a36b80929ad96ba0deeeb737836078efc2a9c91
Artur Dębkowski <arturdebkowski@gmail.com>
2016-12-02 15:07:58 +0100

    percolation data type

---
+++ I/assignment/Percolation.java
@@ -0,0 +1,32 @@
+public class Percolation {
+
+   // create n-by-n grid, with all sites blocked
+   public Percolation(int n) {
+
+   }
+
+   // open site (row, col) if it is not open already
+   public void open(int row, int col) {
+
+   }
+
+   // is site (row, col) open?
+   public boolean isOpen(int row, int col) {
+
+   }
+
+   // is site (row, col) full?
+   public boolean isOpen(int row, int col) {
+
+   }
+
+   // does the system percolate?
+   public boolean percolates() {
+
+   }
+
+   // test client (optional)
+   public static void main(String[] args) {
+
+   }
+}
